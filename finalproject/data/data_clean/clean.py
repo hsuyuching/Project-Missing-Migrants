8efd42ca60bf8c34319e97167b3c7da116fd49b8
William Tang <willtang@Williams-MacBook-Pro.local>
2019-11-23 18:49:08 -0700

    Cleaned and average the missing and death count for the same entry

--- finalproject/data/data_clean/clean.py
+++ finalproject/data/data_clean/clean.py
@@ -6,9 +6,15 @@ df = pd.read_csv('../MissingMigrantsProject.csv')
 df['region_origin'] = df['region_origin'].replace(np.nan, "UNKNOWN")
 df = df.dropna(subset=['cause_of_death'])
 df = df[~df.cause_of_death.str.contains('Unknown', na=False)]
+
+df['dead'].fillna(0, inplace=True)
+df['missing'].fillna(0, inplace=True)
+
+df = df.dropna(subset=['affected_nationality'])
+df = df[~df.affected_nationality.str.contains('Unknown', na=False)]
 """ df['cause_of_death'] = df['cause_of_death'].replace(np.nan, "UNKNOWN") """
-df['affected_nationality'] = df['affected_nationality'].replace(
-    np.nan, "UNKNOWN")
+""" df['affected_nationality'] = df['affected_nationality'].replace(
+    np.nan, "UNKNOWN") """
 df['missing'] = df['missing'].replace(np.nan, "0")
 
 new_df = df.drop('affected_nationality', axis=1).join(
@@ -28,6 +34,11 @@ to_drop = ['source',
            'region_origin',
            'incident_region']
 new_df = new_df.drop(to_drop, axis=1)
+
+new_df['dead'] = new_df.groupby(['id'])['dead'].transform(lambda x: x / len(x))
+new_df['missing'] = new_df.groupby(['id'])['missing'].transform(lambda x: x / len(x))
+
+
 print(new_df)
 # Don't forget to add '.csv' at the end of the path
 export_csv = new_df.to_csv(r'./export_dataframe.csv', index=None, header=True)
