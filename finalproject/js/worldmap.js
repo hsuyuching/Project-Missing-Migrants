9d4d9b0248ded593822ae856d0ab6420d2a36124
Yu-Ching <u1264420@utah.edu>
2019-11-30 17:32:44 -0700

    fix slider and header position

--- finalproject/js/worldmap.js
+++ finalproject/js/worldmap.js
@@ -282,28 +282,30 @@ class worldmap {
     }
     numRounteBar() {
         let that = this
-        let yearScale = d3.scaleLinear().domain([0, 2420]).range([30, 730]);
+        let yearScale = d3.scaleLinear().domain([0, 868]).range([30, 730]);
 
         let yearSlider = d3.select("#routeBar")
             .append("div").classed("slider-wrap", true)
             .append("input").classed("slider", true)
             .attr("type", "range")
             .attr("min", 0)
-            .attr("max", 2420)
+            .attr("max", 868)
             .attr("value", that.activeRoute);
 
         let sliderLabel = d3.select(".slider-wrap")
             .append("div").classed("slider-label", true)
             .append("svg");
 
-        let sliderText = sliderLabel.append('text').text(this.activeRoute);
+        let sliderText = sliderLabel.append('text').text("Show "+this.activeRoute+" routes");
 
         sliderText.attr('x', yearScale(this.activeRoute));
         sliderText.attr('y', 25);
 
         yearSlider.on('input', function () {
-            sliderText.text(this.value);
+            sliderText.text("Show "+this.value+" routes");
             sliderText.attr('x', yearScale(this.value));
+            // sliderText.text("Show number of routes")
+            //     .attr("x", yearScale(this.value));
 
             that.updateRoutePlot(String(this.value));
             that.updateRoute(String(this.value));
@@ -311,52 +313,63 @@ class worldmap {
     }
 
 }
+function reset() {
+    active.classed("active", false);
+    active = d3.select(null);
+
+    g.transition()
+        .duration(750)
+        .style("stroke-width", "1.5px")
+        .attr("transform", "");
+}
+
 function getstartpoint(region) {
     let coord = window.startData.filter(function (d) {
         return d.region == region
     })
     return [coord[0].lon, coord[0].lat]
 }
-function clicked(d) {
-    var x, y, k;
-    //if not centered into that country and clicked country in visited countries
-    if ((d && centered !== d) & (visited_countries.includes(d.id))) {
-        var centroid = path.centroid(d); //get center of country
-        var bounds = path.bounds(d); //get bounds of country
-        var dx = bounds[1][0] - bounds[0][0], //get bounding box
-            dy = bounds[1][1] - bounds[0][1];
-        //get transformation values
-        x = (bounds[0][0] + bounds[1][0]) / 2;
-        y = (bounds[0][1] + bounds[1][1]) / 2;
-        k = Math.min(width / dx, height / dy);
-        centered = d;
-    } else {
-        //else reset to world view
-        x = width / 2;
-        y = height / 2;
-        k = 1;
-        centered = null;
-    }
-    //set class of country to .active
-    g.selectAll("path")
-        .classed("active", centered && function (d) { return d === centered; })
 
-
-    // make contours thinner before zoom for smoothness
-    if (centered !== null) {
-        g.selectAll("path")
-            .style("stroke-width", (0.75 / k) + "px");
-    }
-
-    // map transition
-    g.transition()
-        //.style("stroke-width", (0.75 / k) + "px")
-        .duration(750)
-        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
-        .on('end', function () {
-            if (centered === null) {
-                g.selectAll("path")
-                    .style("stroke-width", (0.75 / k) + "px");
-            }
-        });
-}
+// function clicked(d) {
+//     var x, y, k;
+//     //if not centered into that country and clicked country in visited countries
+//     if ((d && centered !== d) & (visited_countries.includes(d.id))) {
+//         var centroid = path.centroid(d); //get center of country
+//         var bounds = path.bounds(d); //get bounds of country
+//         var dx = bounds[1][0] - bounds[0][0], //get bounding box
+//             dy = bounds[1][1] - bounds[0][1];
+//         //get transformation values
+//         x = (bounds[0][0] + bounds[1][0]) / 2;
+//         y = (bounds[0][1] + bounds[1][1]) / 2;
+//         k = Math.min(width / dx, height / dy);
+//         centered = d;
+//     } else {
+//         //else reset to world view
+//         x = width / 2;
+//         y = height / 2;
+//         k = 1;
+//         centered = null;
+//     }
+//     //set class of country to .active
+//     g.selectAll("path")
+//         .classed("active", centered && function (d) { return d === centered; })
+//
+//
+//     // make contours thinner before zoom for smoothness
+//     if (centered !== null) {
+//         g.selectAll("path")
+//             .style("stroke-width", (0.75 / k) + "px");
+//     }
+//
+//     // map transition
+//     g.transition()
+//         //.style("stroke-width", (0.75 / k) + "px")
+//         .duration(750)
+//         .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+//         .on('end', function () {
+//             if (centered === null) {
+//                 g.selectAll("path")
+//                     .style("stroke-width", (0.75 / k) + "px");
+//             }
+//         });
+// }
